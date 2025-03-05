a9e94637225c89486f00175836554b2585456234
Alexandre <avsouza.es@gmail.com>
2025-03-04 21:18:37 -0300

    CREATE METHODDIV.JAVA
    ADD

---
+++ Java/codegym/nivel_3/MethodDiv.java
@@ -0,0 +1,9 @@
+public class MethodDiv {
+    public static void main(String[] args) {
+        System.out.println(div(10, 2));
+    }
+
+    public static int div(int a, int b) {
+        return a / b;
+    }
+}
