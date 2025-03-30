ed79570157012283371780ed26294856b6caf5f3
Alexandre <avsouza.es@gmail.com>
2025-03-30 19:39:15 -0300

    atualizar gitignore
    atualizando meu gitignore

---
+++ Java/03-arrays/ExemploArrays01.java
@@ -0,0 +1,10 @@
+class ExemploArrays01 {
+    public static void main(String[] args) {
+        double[] notas = new double[2];
+
+        notas[0] = 7.5;
+        notas[1] = 8.2;
+
+        System.out.println(notas[0] + notas[1]);
+    }
+}
